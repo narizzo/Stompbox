0d660ca4179e2a95e32add94c724b2f9a69a196c
Nicholas Rizzo <nrizzo414@gmail.com>
2018-01-05 15:06:17 -0500

    Initial Commit

---
+++ Stompbox_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stompbox_2
+//
+//  Created by Nicholas Rizzo on 1/5/18.
+//  Copyright © 2018 Nicholas Rizzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
