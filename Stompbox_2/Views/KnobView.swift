054119c1126b9056b42489225e64cb3e4eb39443
Nicholas Rizzo <nrizzo414@gmail.com>
2018-02-06 15:58:46 -0500

    SettingCell box now draws properly

--- Stompbox_2/Views/KnobView.swift
+++ Stompbox_2/Views/KnobView.swift
@@ -12,11 +12,14 @@ class KnobView: UIControl {
 
   public override init(frame: CGRect) {
     super.init(frame: frame)
-    
-    backgroundColor = tintColor
+    backgroundColor = UIColor.lightGray
+    layer.borderColor = UIColor.cyan.cgColor
+    layer.borderWidth = 2
+    print("Knob View init from frame:")
   }
   
   public required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+    print("Knob View init from coder:")
+    super.init(coder: aDecoder)
   }
 }
--- Stompbox_2/Views/SettingCell.swift
+++ Stompbox_2/Views/SettingCell.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class SettingCell: UITableViewCell {
   
-  var knobViews: [KnobView]!
+  var knobViews = [KnobView]()
   
+  // gets called if the cell is NOT designed in storyboard
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setup()
+  }
+  
+  // gets called if the cell is ONLY designed in storyboard
+  required init?(coder aDecoder: NSCoder) {
+//    print("SettingCell")
+//   fatalError("init(coder:) has not been implemented")
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  func setup() {
+    knobViews.append(KnobView(frame: self.bounds))
+    print(knobViews.first!.debugDescription)
+    contentView.addSubview(knobViews.first!)
+    //addSubview(knobViews.first!)
+  }
 }
